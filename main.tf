f4f24daf0004ba08667304bce41ff0f9e4d99e85
wxu <xuwillie1@gmail.com>
2026-07-28 21:33:21 -0400

    Remediated S3 vulnerabilities

--- main.tf
+++ main.tf
@@ -23,7 +23,12 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "example" {
     }
   }
 }
-
+resource "aws_s3_bucket_versioning" "versioning" {
+  bucket = aws_s3_bucket.vulnerable_vault.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
 resource "random_id" "id" {  
   byte_length = 4  
 }  
