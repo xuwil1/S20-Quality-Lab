a3f1b2f8ead1c50834952be501b0dea094c59e78
wxu <xuwillie1@gmail.com>
2026-07-28 21:31:31 -0400

    Remediated S3 vulnerabilities

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "aws" {  
   region = "us-east-1"  
 }  
-# tfsec:ignore:aws-s3-enable-bucket-logging
+
 resource "aws_s3_bucket" "vulnerable_vault" {
   bucket = "tkh-exposed-vault-${random_id.id.hex}"
 }  
@@ -13,7 +13,7 @@ resource "aws_s3_bucket_public_access_block" "example" {
   ignore_public_acls      = true
   restrict_public_buckets = true
 }  
-# tfsec:ignore:aws-s3-encryption-customer-key
+
 resource "aws_s3_bucket_server_side_encryption_configuration" "example" {
   bucket = aws_s3_bucket.vulnerable_vault.id
 
@@ -23,12 +23,7 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "example" {
     }
   }
 }
-resource "aws_s3_bucket_versioning" "versioning" {
-  bucket = aws_s3_bucket.vulnerable_vault.id
-  versioning_configuration {
-    status = "Enabled"
-  }
-}
+
 resource "random_id" "id" {  
   byte_length = 4  
 }  
