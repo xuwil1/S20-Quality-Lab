7adcdc1805f5780bff3cbeb24334e81f5dccac4b
wxu <xuwillie1@gmail.com>
2026-07-28 21:35:08 -0400

    Testing SAST Gate

--- main.tf
+++ main.tf
@@ -1,34 +1,12 @@
 provider "aws" {  
   region = "us-east-1"  
 }  
-# tfsec:ignore:aws-s3-enable-bucket-logging
-resource "aws_s3_bucket" "vulnerable_vault" {
-  bucket = "tkh-exposed-vault-${random_id.id.hex}"
+  
+resource "aws_s3_bucket" "vulnerable_vault" {  
+  bucket = "tkh-exposed-vault-${random_id.id.hex}"  
+  acl    = "public-read"  
 }  
-
-resource "aws_s3_bucket_public_access_block" "example" {
-  bucket                  = aws_s3_bucket.vulnerable_vault.id
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
-}  
-# tfsec:ignore:aws-s3-encryption-customer-key
-resource "aws_s3_bucket_server_side_encryption_configuration" "example" {
-  bucket = aws_s3_bucket.vulnerable_vault.id
-
-  rule {
-    apply_server_side_encryption_by_default {
-      sse_algorithm = "AES256"
-    }
-  }
-}
-resource "aws_s3_bucket_versioning" "versioning" {
-  bucket = aws_s3_bucket.vulnerable_vault.id
-  versioning_configuration {
-    status = "Enabled"
-  }
-}
+  
 resource "random_id" "id" {  
   byte_length = 4  
 }  
