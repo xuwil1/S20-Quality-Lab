a1fab03f0ac47887dda8b7d03e88ee7f7363caf0
wxu <xuwillie1@gmail.com>
2026-07-28 21:29:14 -0400

    Remediated S3 vulnerabilities

--- main.tf
+++ main.tf
@@ -6,8 +6,7 @@ resource "aws_s3_bucket" "vulnerable_vault" {
   bucket = "tkh-exposed-vault-${random_id.id.hex}"
 }  
 resource "aws_s3_bucket" "vulnerable_vault" {  
-  bucket = "tkh-exposed-vault-${random_id.id.hex}"  
-  acl    = "public-read"  
+  bucket = "tkh-exposed-vault-${random_id.id.hex}"   
 }  
 resource "aws_s3_bucket_public_access_block" "example" {
   bucket                  = aws_s3_bucket.vulnerable_vault.id
